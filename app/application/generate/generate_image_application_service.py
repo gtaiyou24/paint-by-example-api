ccf60c1f2ed5d14c394ff99bf3720a83d95adafd
gtaiyou24 <gtaiyou24@gmail.com>
2023-10-01 18:56:17 +0900

    feat: generate_image_application_service.py

--- app/application/generate/generate_image_application_service.py
+++ app/application/generate/generate_image_application_service.py
@@ -6,4 +6,4 @@ from application.generate.command import PaintByExampleCommand
 @singleton
 class GenerateImageApplicationService:
     def paint_by_example(self, command: PaintByExampleCommand) -> str:
-        pass
+        return 'https://fujifilm-x.com/wp-content/uploads/2021/01/gfx100s_sample_04_thum-1.jpg'
